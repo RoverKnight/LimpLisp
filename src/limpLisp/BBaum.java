45c19343084936e82dc1835ad579ef0a6aa6b0ed
RoverKnight <lennard.geese@gmx.net>
2021-11-15 18:49:46 +0100

    implemented addKnoten(), fixed ausgebenPost() and ausgebenIn()

--- src/limpLisp/BBaum.java
+++ src/limpLisp/BBaum.java
@@ -18,34 +18,73 @@ public class BBaum {
         return wurzel;
     }
 
-    public void addKnoten(Knoten knoten) {
+    /**
+     * @param knoten Der Knoten der angefuegt werden soll.
+     * @author goose
+     *
+     */
+    public void addElement(Knoten knoten) {
+
+        // fängt aufrufe ab bei denen gegebenes elem gleich null ist; verhindert NullPointerException
+        if (knoten == null) {
+            System.out.println("Smth went wrong");
+            return;
+        }
+
+        // weißt wurzel dem element zu und returnt wenn wurzel leer ist; verhindert Endlosschleife
+        if (wurzel == null) {
+            wurzel = knoten;
+            return;
+        }
+
+        // ausgangsknoten (anchor) wird mit wurzel initialisiert; durchlauf startet bei wurzel
         Knoten anchor = wurzel;
-        boolean shouldRun = true;
-        while (shouldRun) {
 
-            // warn if same
-            if (anchor.getInhalt() == knoten.getInhalt()) {
-                System.out.println("Knots have same value");
+        // durchläuft baum bis freier platz gefunden wird, returnt wenn gefunden u. angefuegt
+        while (true) {
+
+            // wenn daten des elems kleiner/gleich dem ausgangsknoten sind
+            if (knoten.getInhalt() <= anchor.getInhalt()) {
+                if (anchor.getLeft() == null) {
+                    anchor.setLeft(knoten);
+                    return;
+                }
+                else {
+                    anchor = anchor.getLeft();
+                }
             }
 
-            // if smaller
-            if (anchor.getInhalt() < knoten.getInhalt()) {
-
-            }
-
-            // if bigger
+            // wenn daten des elems größer als ausgangsknoten sind
             else {
-
+                if (anchor.getRight() == null) {
+                    anchor.setRight(knoten);
+                    return;
+                }
+                else {
+                    anchor = anchor.getRight();
+                }
             }
         }
+
     }
 
     public void deleteKnoten(Knoten knoten) {
 
     }
 
-    // TODO
-    public void ausgebenPre(Knoten knoten) {
+    public void ausgebenPre() {
+        ausgebenPre(getWurzel());
+    }
+
+    public void ausgebenPost() {
+        ausgebenPost(getWurzel());
+    }
+
+    public void ausgebenIn() {
+        ausgebenIn(getWurzel());
+    }
+
+    private void ausgebenPre(Knoten knoten) {
         System.out.println(knoten.getInhalt());
         try {
             ausgebenPre(knoten.getLeft());
@@ -56,58 +95,55 @@ public class BBaum {
         } catch (NullPointerException ignored) {}
     }
 
-    // TODO
-    public void ausgebenPost(Knoten knoten) {
+    private void ausgebenPost(Knoten knoten) {
         try {
-            ausgebenPre(knoten.getLeft());
+            ausgebenPost(knoten.getLeft());
         } catch (NullPointerException ignored) {}
 
         try {
-            ausgebenPre(knoten.getRight());
+            ausgebenPost(knoten.getRight());
         } catch (NullPointerException ignored) {}
 
         System.out.println(knoten.getInhalt());
     }
 
-    // TODO
-    public void ausgebenIn(Knoten knoten) {
+    private void ausgebenIn(Knoten knoten) {
         try {
-            ausgebenPre(knoten.getLeft());
+            ausgebenIn(knoten.getLeft());
         } catch (NullPointerException ignored) {}
 
         System.out.println(knoten.getInhalt());
 
         try {
-            ausgebenPre(knoten.getRight());
+            ausgebenIn(knoten.getRight());
         } catch (NullPointerException ignored) {}
 
     }
 
     public Knoten getByPfad(boolean[] directions) {
-
+        return null;
     }
 
     public Knoten[] getKnotenPre() {
-
+        return null;
     }
 
     public Knoten[] getKnotenPost() {
-
+        return null;
     }
 
     public Knoten[] getKnotenIn() {
-
+        return null;
     }
 
     public int berechneWidth() {
-
+        return 0;
     }
 
     public int berechneDepth() {
-
+        return 0;
     }
 
-    // TODO
     public void clear() {
         wurzel = null;
     }
