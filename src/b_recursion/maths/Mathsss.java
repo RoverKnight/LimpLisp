615e16f4205e9928177cd6e83e1f57615d93de9b
RoverKnight <lennard.geese@gmx.net>
2021-12-10 20:41:24 +0100

    added fibonacci calculations and testing for functionality and time efficiency

--- src/b_recursion/maths/Mathsss.java
+++ src/b_recursion/maths/Mathsss.java
@@ -4,21 +4,8 @@ public class Mathsss {
 
 
 
-
-
-    public static double facultyRecursive(double num) {
-        /* return 1 if num is 0 or 1;
-         * not catching 0 will cause StackOverflowError
-         * 1! is (obviously) 1
-         */
-        if (num == 1 || num == 0) return 1;
-
-
-        return num * facultyRecursive(num - 1);
-    }
-
-    public static double facultyIterative(double num) {
-        // return 1 if num is 0; 0! is 1 but uncomputable
+    public static double facultyI(double num) {
+        // 0! is 1 but uncomputable, must be handled separately
         if (num == 0) return 1;
 
         // compute via for-loop
@@ -28,6 +15,42 @@ public class Mathsss {
         }
 
         return num;
+    }
+
+    public static double facultyR(double num) {
+        // not catching 0 will cause StackOverflowError and 1! is 1
+        if (num == 1 || num == 0) return 1;
+
+        // a^x == a * a^(x-1)
+        return num * facultyR(num - 1);
+    }
+
+    public static double fibonacciI(int index) {
+        if (index < 0) {
+            System.out.println(":|");
+            return -1;
+        }
+        else if (index == 0) return 0;
+
+        double num = 1;
+        double oldNum = 0;
+        for (int i = 1; i < index; i++) {
+            num += oldNum;
+            oldNum = num - oldNum;
+        }
+
+        return num;
+    }
+
+    public static double fibonacciR(int index) {
+        if (index < 0) {
+            System.out.println(":|");
+            return -1;
+        }
+        else if (index == 0) return 0;
+        else if (index == 1) return 1;
+
+        return fibonacciR(index - 1) + fibonacciR(index - 2);
     }
 
 }
--- src/b_recursion/maths/MathsssTest.java
+++ src/b_recursion/maths/MathsssTest.java
@@ -6,16 +6,44 @@ public class MathsssTest {
 
 
 
-
-
-
-
     @Test
     public void facultyTest() {
         int num = 55;
-        System.out.println(num + "! = " + Mathsss.facultyRecursive(num));
+        System.out.println(num + "! = " + Mathsss.facultyR(num));
 
-        System.out.println(num + "! = " + Mathsss.facultyIterative(num));
+        System.out.println(num + "! = " + Mathsss.facultyI(num));
+    }
+
+    @Test
+    public void fibonacciFunctionalityTest() {
+        System.out.println("I:");
+        for (int i = 0; i < 45; i++) {
+            System.out.println("  #" + i + " - " + Mathsss.fibonacciI(i));
+        }
+
+        System.out.println("R:");
+        for (int i = 0; i < 45; i++) {
+            System.out.println("  #" + i + " - " + Mathsss.fibonacciR(i));
+        }
+    }
+
+    @Test
+    public void fibonacciTimedTest() {
+        int[] timeStamps = { 5, 25, 30, 31, 33, 35, 40, 45 };
+        for (int i = 0; i < timeStamps.length; i++) {
+            System.out.println("index = " + timeStamps[i] + ":");
+            long startTime = System.currentTimeMillis();
+            Mathsss.fibonacciI(timeStamps[i]);
+            long endTime = System.currentTimeMillis();
+            long timeTaken = endTime - startTime;
+            System.out.println("  I - " + timeTaken + "ms");
+
+            startTime = System.currentTimeMillis();
+            Mathsss.fibonacciR(timeStamps[i]);
+            endTime = System.currentTimeMillis();
+            timeTaken = endTime - startTime;
+            System.out.println("  R - " + timeTaken + "ms");
+        }
     }
 
 }
