e2d90a9a0318f1e5ff24d9685a73485fa9d5c518
RoverKnight <lennard.geese@gmx.net>
2021-12-15 10:58:41 +0100

    started iterative towers of hanoi

---
+++ src/b_recursion/towersOfHanoi/Plate.java
@@ -0,0 +1,21 @@
+package b_recursion.towersOfHanoi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Plate {
+
+    private int towerIndex = 0;
+
+    public Plate(int towerIndex) {
+        this.towerIndex = towerIndex;
+
+    }
+
+
+
+
+
+
+
+}
--- src/b_recursion/towersOfHanoi/TowersOfHanoi.java
+++ src/b_recursion/towersOfHanoi/TowersOfHanoi.java
@@ -1,5 +1,7 @@
 package b_recursion.towersOfHanoi;
 
+import a_dataStructures.linkedLists.Stack;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -11,14 +13,14 @@ import javax.swing.*;
  * @version 1.0 vom 15.09.2010
  */
 
-public class towersOfHanoi extends JFrame {
+public class TowersOfHanoi extends JFrame {
     // Anfang Attribute
     private final JTextField jTextField1 = new JTextField();
     private final JButton jButton1 = new JButton();
     int count = 0;
     // Ende Attribute
 
-    public towersOfHanoi(String title) {
+    public TowersOfHanoi(String title) {
 
         // Frame-Initialisierung - GUI Fenster
         super(title);
@@ -51,7 +53,6 @@ public class towersOfHanoi extends JFrame {
         setVisible(true);
     }
 
-    // Anfang Methoden
     public void jButton1_ActionPerformed(ActionEvent evt) {
         int n = Integer.parseInt(jTextField1.getText());
         count = 0;
@@ -65,27 +66,48 @@ public class towersOfHanoi extends JFrame {
      * @param numOfPlates The number of plates
      * @param t1 index of starting tower
      * @param t2 index of transition tower
-     * @param t3 index of final tower
+     * @param t3 index of destination tower
      */
     public void hanoi(int numOfPlates, int t1, int t2, int t3) {
-
+        // set transition tower as destination tower
         if (numOfPlates > 1) hanoi(numOfPlates - 1, t1, t3, t2);
 
         System.out.println(t1 + " -> " + t3);
 
+        // set transition tower as starting tower
         if (numOfPlates > 1) hanoi(numOfPlates - 1, t2, t1, t3);
-
-
-        /*
-         keine rückkehr operation
-         gerade nur auf ungerade, ungerade nur auf gerade
-         größtmögliche auf freien stab wenn keine anderen züge möglich
-         */
     }
 
-    // Ende Methoden
+    public void hanoiI(int numOfPlates) {
+
+        Stack[] towers = {
+                new Stack(),
+                new Stack(),
+                new Stack()
+        };
+
+        int i = 0;
+        while (!towers[0].empty() && !towers[1].empty()) {
+
+            if (i % 2 == 1) {
+
+
+
+            }
+
+
+
+
+
+            i++;
+        }
+
+    }
+
+
+
 
     public static void main(String[] args) {
-        new towersOfHanoi("towersOfHanoi");
+        new TowersOfHanoi("TowersOfHanoi");
     }
 }
