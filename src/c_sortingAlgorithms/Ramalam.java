0ceb341e9032e9d87de3ab4f8b06467430f9a800
RoverKnight <lennard.geese@gmx.net>
2022-01-18 21:00:34 +0100

    successfully implemented badCocktailSort and bubbleSort

--- src/c_sortingAlgorithms/Ramalam.java
+++ src/c_sortingAlgorithms/Ramalam.java
@@ -2,6 +2,7 @@ package c_sortingAlgorithms;
 
 import x_randomShit.FuckYouException;
 
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,56 +29,90 @@ public class Ramalam {
 
     }
 
-    public static int[] bubbleSort(int[] unsortedArray) {
+    public static int[] bubbleSort(int[] array) {
 
+        /* gesamtes array durchlaufen bis sortiert ist
+         * (ausgangsbedingung: keinen tausch durchführen; bei tausch wird i zurückgesetzt)
+         */
+        for (int i = 1; i < array.length; i++) {
+
+            // werte tauschen falls der rechte kleiner als sein linker nachbar ist
+            if (array[i - 1] > array[i]) {
+                int temp = array[i]; // hilfsvariable zum werte tauschen
+                array[i] = array[i - 1];
+                array[i - 1] = temp;
+
+                // von vorne anfangen
+                i = 0; // i muss gleich 0 (nicht 1) gesetzt werden, da i im nächsten durchlauf inkrementiert wird
+            }
+        }
+
+        return array;
     }
 
     public static int[] insertionSort(int[] unsortedArray) {
-
+        return new int[]{0};
     }
 
     public static int[] selectionSort(int[] unsortedArray) {
-
+        return new int[]{0};
     }
 
-    public static int[] wrongCocktailSort(int[] unsortedArray) {
+    /** überträgt inhalte eines arrays in eine liste; anordnung der inhalte wird nicht berücksichtigt! */
+    public static List<Integer> convertToList(int[] array) {
+        List<Integer> list = new ArrayList<>();
+        for (int j : array) {
+            list.add(j);
+            //System.out.println("A - " + array[i] + " ::: L - " + list.get(i));
+        }
+        return list;
+    }
+
+    public static int[] badCocktailSort(int[] unsortedArray) {
 
         // variablen vorbereiten
-        int highestIndex = 0;
-        int lowestIndex = 0;
-        List<Integer> highest = new ArrayList<Integer>();
-        List<Integer> lowest = new ArrayList<Integer>();
-        List<Integer> unsorted = new ArrayList<Integer>();
+        int highestIndex;
+        int lowestIndex;
+        List<Integer> highest = new ArrayList<>();
+        List<Integer> lowest = new ArrayList<>();
+        List<Integer> unsorted;
         int[] sorted = new int[unsortedArray.length];
 
         // gegebenes array zu liste konvertieren
-        for (int num : unsortedArray) {
-            unsorted.add(num);
-        }
+        unsorted = convertToList(unsortedArray);
 
-        // elemente in zwei sortierte listen sortieren
+        // elemente in zwei listen einsortieren
         while (unsorted.size() > 0) {
 
-            // unsortierte menge nach größtem/kleinstem wert parsen
+            // indexe zurücksetzen um IndexOutOfBoundsException vorzubeugen
+            highestIndex = 0;
+            lowestIndex = 0;
+
+            // unsortierte menge nach größtem/kleinstem wert durchsuchen, indexe merken
             for (int i = 1; i < unsorted.size(); i++) {
                 if (unsorted.get(i) < unsorted.get(lowestIndex)) lowestIndex = i;
                 else if (unsorted.get(i) > unsorted.get(highestIndex)) highestIndex = i;
             }
 
             // größten/kleinsten wert der entsprechenden menge anfügen
-            lowest.add(unsorted.get(lowestIndex));
-            highest.add(0, unsorted.get(highestIndex));
+            if (unsorted.size() != 1) {
+                lowest.add(unsorted.get(lowestIndex));
+                highest.add(0, unsorted.get(highestIndex));
+            }
+            else lowest.add(unsorted.get(0));
 
-            // größten/kleinsten wert aus der unsortierten menge entfernen
-            // FIXME(?): wenn indexe gleich..?
+            /* größten/kleinsten wert aus der unsortierten menge entfernen
+             * (if-bedingungen verhindern, dass durch aufrückende elemente das falsche aussortiert wird)
+             */
             if (lowestIndex < highestIndex) {
                 unsorted.remove(highestIndex);
                 unsorted.remove(lowestIndex);
             }
-            else {
+            else if (lowestIndex > highestIndex) {
                 unsorted.remove(lowestIndex);
                 unsorted.remove(highestIndex);
             }
+            else unsorted.remove(0);
         }
 
         // sortierte listen zusammenführen
@@ -87,6 +122,18 @@ public class Ramalam {
         for (int i = 0; i < lowest.size(); i++) {
             sorted[i] = lowest.get(i);
         }
+
+        /* COOLER CONVERSION METHOD >:D
+        int i = 0;
+        try {
+            while (true) {
+                sorted[i] = lowest.get(0);
+                lowest.remove(0);
+                i++;
+            }
+        } catch (IndexOutOfBoundsException ignored) {}
+        */
+
         return sorted;
 
     }
@@ -96,6 +143,29 @@ public class Ramalam {
             if (array[i] > array[i + 1]) return false;
         }
         return true;
+    }
+
+    public static void printArray(int[] array, String identifier) {
+        if (!identifier.isEmpty()) System.out.print(identifier + " ");
+        for (int i = 0; i < array.length; i++) {
+            if (i != array.length - 1) System.out.print(array[i] + ", ");
+            else System.out.print(array[i]);
+        }
+        System.out.println();
+    }
+
+    public static void printArray(int[] array, String identifier, int indent) {
+        String indentStr = "";
+        for (int i = 0; i < indent; i++) {
+            indentStr += " ";
+        }
+
+        if (!identifier.isEmpty()) System.out.print(indent + identifier + " ");
+        for (int i = 0; i < array.length; i++) {
+            if (i != array.length - 1) System.out.print(array[i] + ", ");
+            else System.out.print(array[i]);
+        }
+        System.out.println();
     }
 
 }
--- src/c_sortingAlgorithms/RamalamTest.java
+++ src/c_sortingAlgorithms/RamalamTest.java
@@ -1,9 +1,25 @@
 package c_sortingAlgorithms;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import x_randomShit.FuckYouException;
 
+import java.util.Arrays;
+
 public class RamalamTest {
+
+    int[] unsortedArray;
+    int[] sortedArray;
+
+    @BeforeEach
+    public void randomizeInput() {
+        unsortedArray = new int[(int) (Math.random() * 25)];
+        sortedArray = new int[unsortedArray.length];
+        for (int i = 0; i < unsortedArray.length; i++) {
+            unsortedArray[i] = (int) (Math.random() * 100);
+            //if (Math.random() < 0.3) unsortedArray[i] *= -1;
+        }
+    }
 
     @Test
     public void searchMeTest() {
@@ -31,5 +47,19 @@ public class RamalamTest {
         } catch (FuckYouException ignored) {
             System.out.println("Shit");
         }
+    }
+
+    @Test
+    public void badCocktailSortTest() {
+        Ramalam.printArray(unsortedArray, "Unsorted:");
+        sortedArray = Ramalam.badCocktailSort(unsortedArray);
+        Ramalam.printArray(sortedArray, "Sorted: ");
+    }
+
+    @Test
+    public void bubbleSortTest() {
+        Ramalam.printArray(unsortedArray, "Unsorted:");
+        sortedArray = Ramalam.bubbleSort(unsortedArray);
+        Ramalam.printArray(sortedArray, "Sorted: ");
     }
 }
