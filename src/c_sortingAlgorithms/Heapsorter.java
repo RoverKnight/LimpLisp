06d817f36fc293f6145cb0d1a8cb6e8e10556198
RoverKnight <lennard.geese@gmx.net>
2022-02-03 23:08:12 +0100

    successfully implemented heapsort

--- src/c_sortingAlgorithms/Heapsorter.java
+++ src/c_sortingAlgorithms/Heapsorter.java
@@ -1,5 +1,12 @@
 package c_sortingAlgorithms;
 
+import a_dataStructures.linkedLists.Element;
+import a_dataStructures.trees.BBaum;
+import a_dataStructures.trees.Knoten;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Heapsorter {
 
 
@@ -12,23 +19,48 @@ public class Heapsorter {
 
 
     public int[] sort() {
-
-
-
-
-
-
-        return new int[] {0};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("call " + i);
+            heapify(i, 0, i);
+            Ramalam.printArray(array, "=> ");
+        }
+        return array;
     }
 
-    public int[] heapify(int[] array, int rootIndex) {
+    private void heapify(int subRootIndex, int depth, int offset) {
 
+        if (subRootIndex >= array.length) return;
 
+        System.out.println("  heapify " + array[subRootIndex] + " @ [" + subRootIndex + "] - " + depth);
 
+        // rekursives aufrufen fuer kinderelemente
+        int c1 = (subRootIndex - offset) * 2 + offset + 1;
+        int c2 = c1 + 1;
+        heapify(c1, depth + 1, offset);
+        System.out.println("  (next child)");
+        heapify(c2, depth + 1, offset);
 
+        System.out.println("  back to " + array[subRootIndex] + " @ [" + subRootIndex + "] - " + depth);
 
+        // sortieren innerhalb des 3-element heaps; groeßtes element kommt nach oben
+        try {
 
-        return new int[] {0};
+            // mit linkem kind tauschen falls dieses größer
+            if (array[c1] > array[subRootIndex]) {
+                System.out.println("    switching " + array[subRootIndex] + " @ [" + subRootIndex + "] w/ " + array[c1] + " @ [" + c1 + "]");
+                int temp = array[c1];
+                array[c1] = array[subRootIndex];
+                array[subRootIndex] = temp;
+            }
+
+            // mit rechtem kind tauschen falls dieses größer
+            if (array[c2] > array[subRootIndex]) {
+                System.out.println("    switching " + array[subRootIndex] + " @ [" + subRootIndex + "] w/ " + array[c2] + " @ [" + c2 + "]");
+                int temp = array[c2];
+                array[c2] = array[subRootIndex];
+                array[subRootIndex] = temp;
+            }
+        } catch (ArrayIndexOutOfBoundsException ignored) {}
     }
 
 
--- src/c_sortingAlgorithms/RamalamTest.java
+++ src/c_sortingAlgorithms/RamalamTest.java
@@ -11,7 +11,8 @@ public class RamalamTest {
 
     @BeforeEach
     public void randomizeInput() {
-        unsortedArray = new int[(int) (Math.random() * 25)];
+        //unsortedArray = new int[(int) (Math.random() * 10)];
+        unsortedArray = new int[7];
         sortedArray = new int[unsortedArray.length];
         for (int i = 0; i < unsortedArray.length; i++) {
             unsortedArray[i] = (int) (Math.random() * 100);
@@ -85,6 +86,25 @@ public class RamalamTest {
         Heapsorter heap = new Heapsorter(unsortedArray);
         sortedArray = heap.sort();
         Ramalam.printArray(sortedArray, "Sorted: ");
-        assert Ramalam.isSorted(sortedArray);
+        assert Ramalam.isSortedReverse(sortedArray);
+    }
+
+    @Test
+    public void heapSortSuperTest() {
+        Ramalam.printArray(unsortedArray, "Unsorted:");
+        Heapsorter heap = new Heapsorter(unsortedArray);
+        sortedArray = heap.sort();
+        Ramalam.printArray(sortedArray, "Sorted: ");
+        while (Ramalam.isSortedReverse(sortedArray)) {
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            randomizeInput();
+            Ramalam.printArray(unsortedArray, "Unsorted:");
+            System.out.println();
+            heap = new Heapsorter(unsortedArray);
+            sortedArray = heap.sort();
+        }
+        assert Ramalam.isSortedReverse(sortedArray);
     }
 }
